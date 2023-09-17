e3e205cb5af0eeb0ef6d286e74912227d6ca91de
vahidalizadeh7070 <vahidalizadeh7070@gmail.com>
2023-09-18 01:29:57 +0330

    test(Controller) GetAll endpoint - add 200ok result test for GetAll - add 204nocontent result test for GetAll - add NotEmpty test for GetAll

---
+++ PostService.Test/Dummy/PostDummyData.cs
@@ -0,0 +1,28 @@
+﻿using PostService.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostService.Test.Dummy
+{
+    public class PostDummyData
+    {
+        public PostDummyData()
+        {
+
+        }
+
+        public IEnumerable<Post> GetAllPost()
+        {
+            var posts = new List<Post>();
+            posts.Add(new Post { Id = "FBFF1432-05BC-4686-A888-90B86A70D07C", Title = "test1", UserName = "test1", CreatedAt = DateTime.Now });
+            posts.Add(new Post { Id = "FBFF1432-05BC-4686-A888-90B86A70D07D", Title = "test2", UserName = "test2", CreatedAt = DateTime.Now });
+            posts.Add(new Post { Id = "FBFF1432-05BC-4686-A888-90B86A70D07E", Title = "test3", UserName = "test3", CreatedAt = DateTime.Now });
+            posts.Add(new Post { Id = "FBFF1432-05BC-4686-A888-90B86A70D07F", Title = "test4", UserName = "test4", CreatedAt = DateTime.Now });
+            posts.Add(new Post { Id = "FBFF1432-05BC-4686-A888-90B86A70D07G", Title = "test5", UserName = "test5", CreatedAt = DateTime.Now });
+            return posts;
+        }
+    }
+}
---
+++ PostService.Test/TestController/GetAll_PostController.cs
@@ -0,0 +1,84 @@
+﻿using MapsterMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PostService.API.Controllers;
+using PostService.API.DTO;
+using PostService.API.Models;
+using PostService.API.Services;
+using PostService.Test.Dummy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostService.Test.TestController
+{
+    public class GetAll_PostController
+    {
+        private Mock<IMapper> _mapper;
+        private Mock<IPostServices> _postServices;
+        public GetAll_PostController()
+        {
+            _mapper = new Mock<IMapper>();
+            _postServices = new Mock<IPostServices>();  
+        }
+
+        [Fact]
+        public async Task GetAll_PostController_NotEmpty()
+        {
+            // Arrange
+            var data = new PostDummyData();
+            _postServices.Setup(service => service.GetAllAsync().Result).Returns(data.GetAllPost);
+            _mapper.Setup(mapper => mapper.Map<IEnumerable<ResponsePostDTO>>(It.IsAny<IEnumerable<Post>>()));
+
+            var controller = new PostController(_mapper.Object,_postServices.Object);
+
+            // Act
+            var res = await controller.Get();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(res);
+            var model = Assert.IsAssignableFrom<IEnumerable<ResponsePostDTO>>(okResult.Value);
+            Assert.NotNull(model);
+        }
+
+        [Fact]
+        public async Task GetAll_PostController_204NoContent()
+        {
+            // Arrange
+            var data = new PostDummyData();
+            _postServices.Setup(service => service.GetAllAsync().Result).Returns(data.GetAllPost().Where(p=>p.Id ==""));
+            _mapper.Setup(mapper => mapper.Map<IEnumerable<ResponsePostDTO>>(It.IsAny<IEnumerable<Post>>()));
+
+            var controller = new PostController(_mapper.Object, _postServices.Object);
+
+            // Act
+            var res = await controller.Get();
+
+            // Assert
+            var noContentResult = Assert.IsType<NoContentResult>(res);
+            Assert.Equal(StatusCodes.Status204NoContent, noContentResult.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAll_PostController_200Ok()
+        {
+            // Arrange
+            var data = new PostDummyData();
+            _postServices.Setup(service => service.GetAllAsync().Result).Returns(data.GetAllPost);
+            _mapper.Setup(mapper => mapper.Map<IEnumerable<ResponsePostDTO>>(It.IsAny<IEnumerable<Post>>()));
+
+            var controller = new PostController(_mapper.Object, _postServices.Object);
+
+            // Act
+            var res = await controller.Get();
+
+            // Assert
+            var noContentResult = Assert.IsType<OkObjectResult>(res);
+            Assert.Equal(StatusCodes.Status200OK, noContentResult.StatusCode);
+        }
+    }
+}
